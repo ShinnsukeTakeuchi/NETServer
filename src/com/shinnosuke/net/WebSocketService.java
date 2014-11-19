ce232e7dee4d06942726893fe2834e90a54a2aa5
ShinnsukeTakeuchi <shinnosuke.takeuchi0921@gmail.com>
2014-11-19 18:32:47 +0900

    再スタート前コミット

--- src/com/shinnosuke/net/WebSocketService.java
+++ src/com/shinnosuke/net/WebSocketService.java
@@ -2,6 +2,10 @@ package com.shinnosuke.net;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
@@ -12,13 +16,15 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint(value="/Post")
 public class WebSocketService {
 	private static ArrayList<Session> sessionList = new ArrayList<Session>();
+	private static Map<Integer,ArrayList<Session>> sessions = new HashMap<Integer, ArrayList<Session>>();
 
     @OnOpen
     public void onStart(Session session){
         try{
             sessionList.add(session);
+//            sessions.get(1).add(session);
             //asynchronous communication
-            session.getBasicRemote().sendText("Hello!");
+            session.getBasicRemote().sendText("〇〇さんが入室しました。");
         }catch(IOException e){}
     }
 
@@ -28,12 +34,32 @@ public class WebSocketService {
     }
 
     @OnMessage
-    public void onMessage(String msg){
+    public void onMessage(String msg, Session getSession){
+    	Set<Session> sessions = getSession.getOpenSessions();
         try{
-            for(Session session : sessionList){
+//            for(Session session : sessionList){
+        	for(Session session : sessions){
                 //asynchronous communication
-                session.getBasicRemote().sendText("Server:"+msg);
+                session.getBasicRemote().sendText(msg);
             }
         }catch(IOException e){}
+    }
+
+    class Room {
+    	private String roomName;
+    	private List<Session> users;
+    	public void sendUser(String userNamem) {
+
+    	}
+
+    	public void sendAll(String msg) throws IOException {
+    		for(Session session : users){
+                session.getBasicRemote().sendText(msg);
+            }
+    	}
+
+    	public void addSession(Session session) {
+    		users.add(session);
+    	}
     }
 }
